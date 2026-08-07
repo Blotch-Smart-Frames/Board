830d78dfddf06bf49a0cdb7202c2e6154b1e8cbc
Dolan Miu <dolan_miu@hotmail.com>
2026-08-07 11:36:41 +0100

    feat: Remove sample schedule code

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,6 +1,5 @@
 // Firebase Functions entry point. Every export at the top level becomes a
 // deployable function. Re-export new functions from their module here.
 
-export { dailyCleanup } from "./scheduled/daily-cleanup";
 export { escalatePastDue } from "./scheduled/escalate-past-due";
 export { cleanupDeletedBoard } from "./triggers/cleanup-deleted-board";
